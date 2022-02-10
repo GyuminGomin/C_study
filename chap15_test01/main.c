af2768a6540298a4a2872f420a7c81753da1ec66
GyuminGomin <ool9898@naver.com>
2022-02-10 13:53:03 +0900

    second commit

--- chap15_test01/main.c
+++ chap15_test01/main.c
@@ -5,8 +5,17 @@ void main()
 {
 	char data[5] = { 0, }; // 지역변수에는 무조건 초기화를 해야한다! 안하면 garbage값들이 들어가있으므로
 	data[1] = 5;
+	*(data + 2) = 15;
+
 	for (int i = 0; i < sizeof(data); i++)
 	{
-		printf("%d\n", data[i]);
+		printf("%d\t", data[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < sizeof(data); i++)
+	{
+		printf("%p\n", &data[i]);
+		printf("%p\n\n", (data + i));
 	}
 }
