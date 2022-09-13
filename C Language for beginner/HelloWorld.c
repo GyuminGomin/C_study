0b929c4745df69f02da6cdd2e9750388abfec7cb
GyuminGomin <ool9898@naver.com>
2022-09-13 17:31:54 +0900

    first commit

---
+++ C Language for beginner/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char greet[2];
+    printf("Hello World! \n");
+    scanf("%s",greet);
+    return 0;
+}
+
+// 빌드 버튼 (ctrl + alt + c)
+// run 버튼 (ctrl + alt + r)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 C 기초 비교과 was writed by visual studio 2019 editor  
-C Language for beginner was writed by visual studio editor
+C Language for beginner was writed by visual studio code editor
